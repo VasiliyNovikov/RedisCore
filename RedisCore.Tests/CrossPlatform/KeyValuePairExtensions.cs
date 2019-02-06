d62e9b90660d417ce96d4ef9262a88179b10bed2
Vasiliy Novikov <vanoviko@microsoft.com>
2019-02-07 00:36:46 +0200

    More .NET Standard compatibility fixes

---
+++ RedisCore.Tests/CrossPlatform/KeyValuePairExtensions.cs
@@ -0,0 +1,17 @@
+﻿#if NETSTANDARD
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace RedisCore.Tests
+{
+    public static class KeyValuePairExtensions
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static void Deconstruct<TKey, TValue>(this in KeyValuePair<TKey, TValue> keyValuePair, out TKey key, out TValue value)
+        {
+            key = keyValuePair.Key;
+            value = keyValuePair.Value;
+        }
+    }
+}
+#endif
---
+++ RedisCore.Tests/CrossPlatform/RandomExtensions.cs
@@ -0,0 +1,15 @@
+﻿#if NETSTANDARD
+using System;
+
+namespace RedisCore.Tests
+{
+    public static class RandomExtensions
+    {
+        public static void NextBytes(this Random random, Span<byte> buffer)
+        {
+            for (var i = 0; i < buffer.Length; ++i)
+                buffer[i] = (byte)random.Next();
+        }
+    }
+}
+#endif
