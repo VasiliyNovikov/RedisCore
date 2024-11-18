a3f49123f438ca2702e03f11f2187a76d26d489d
Vasyl Novikov <vanoviko@microsoft.com>
2024-11-18 09:45:50 +0200

    Allocation reduction

--- RedisCore/Internal/Commands/OptionalValueCommand.cs
+++ RedisCore/Internal/Commands/OptionalValueCommand.cs
@@ -1,5 +1,6 @@
 ﻿using RedisCore.Internal.Protocol;
+using System;
 
 namespace RedisCore.Internal.Commands;
 
-internal abstract class OptionalValueCommand<T>(RedisString name, params RedisObject[] args) : Command<Optional<T>>(name, args);
+internal abstract class OptionalValueCommand<T>(RedisString name, params ReadOnlySpan<RedisObject> args) : Command<Optional<T>>(name, args);
